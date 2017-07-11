6e7adb50e65a0bb6c01c78d8c27039f5e8791fda
Prachi <prachi.bhatkar@bynry.com>
2017-07-11 15:22:27 +0530

    Intial commit

---
+++ app/src/main/java/com/bynry/btcl_employeeengagementapp/ui/activity/MainActivity.java
@@ -0,0 +1,19 @@
+package com.bynry.btcl_employeeengagementapp.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bynry.btcl_employeeengagementapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // main activity
+
+
+
+    }
+}
